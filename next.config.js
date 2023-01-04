c6ba8bcaafbcb22eed8fca4599a586e4ec327da4
Gerardo Valenzuela <gjvalenz@usc.edu>
2023-01-03 21:46:49 -0800

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  basePath: '/gerardvee-website',
+  assetPrefix: '/gerardvee-website'
 }
 
 module.exports = nextConfig
