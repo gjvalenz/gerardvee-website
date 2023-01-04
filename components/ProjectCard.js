93ccb1bae8c91c40c0c10ca772a6d7bd518e88ad
Gerardo Valenzuela <gjvalenz@usc.edu>
2023-01-04 02:17:44 -0800

    fetch from rtd

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -5,8 +5,7 @@ import styles from '../styles/components/ProjectCard.module.css'
 import stylesIndex from '../styles/project/pid/index.module.css'
 import stylesLink from '../styles/components/Link.module.css'
 
-
-function ReadMore({clicked, callback, short_description, skills_shown, github_url, url}) {
+function ReadMore({clicked, callback, short_description, skills, github_url, url}) {
   if(!clicked)
     return (
       <p className={stylesLink.smlink} url={''} onClick={() => callback(true)}>Read more</p>
@@ -14,7 +13,7 @@ function ReadMore({clicked, callback, short_description, skills_shown, github_ur
   return (
     <>
       <p className={styles.description}>{short_description}</p>
-      <p className={styles.skills}>tech: {skills_shown}</p>
+      <p className={styles.skills}>tech: {skills}</p>
       <div className={stylesIndex.sideBySide}>
         <Link small url={github_url}>github</Link>
         { url && <Link small url={url}>demo</Link> }
@@ -35,7 +34,6 @@ export default class ProjectCard extends React.Component
   render(){
     let {_id, story, url, short_description, name, github_url, skills, image_url} = this.props;
     let id = _id;
-    let skills_shown = skills.join(', ');
     return (
     <div className={styles.container} data-project-id={id}>
         { url && <Link big url={url}>{name}</Link> }
@@ -43,7 +41,7 @@ export default class ProjectCard extends React.Component
         <img src={image_url} height={320} width={640}/>
         <p className={stylesIndex.story}>{story}</p>
         <ReadMore clicked={this.state.clicked} callback={(bool) => {this.setState({clicked: bool});}}
-          short_description={short_description} skills_shown={skills_shown} github_url={github_url} url={url} />
+          short_description={short_description} skills={skills} github_url={github_url} url={url} />
     </div>);
   }
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import ProjectCard from '../components/ProjectCard'
 import WebsiteBase from '../components/WebsiteBase'
-import { getDocs, collection } from 'firebase/firestore'
-import db from '../projects_db_instance'
 
 function Projects({projects}) {
   return (
@@ -13,36 +11,19 @@ function Projects({projects}) {
   );
 }
 
-//export async function getServerSideProps() { use static props rather than serversideprops
 Projects.getInitialProps = async (ctx) => { 
-  //const res = await fetch('https://www.gerardvee.com/api/projects')
-  //const snapshot = await onValue(ref);
-  //const data = await getData(ref);
-  const res = await fetch('https://firestore.googleapis.com/v1/projects/website-373507/databases/(default)/documents/projects');
-  const projects = await res.json();
-  console.log(projects);
-  let dats = [];
-  for(let key in projects)
+  const res = await fetch('https://website-373507-default-rtdb.firebaseio.com/projects.json')
+  const data = await res.json()
+  let projects = []
+  for(let key in data)
   {
-    if(projects.hasOwnProperty(key))
+    if(data.hasOwnProperty(key))
     {
-      dats.push(projects[key]);
+      projects.push(data[key])
     }
   }
-  /*qs.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-      dats.push(doc.data());
-  })*/
-  //ref.once('value', (data) => {
-  //  console.log(data);
-  //});
-  //console.log(data.val());
-  //const snapshot = await ref.once('value')
-  //const val = snapshot.val()
-  //console.log(val)
-  //const projects = await res.json()
 
-  return { projects: dats }
+  return { projects }
 }
 
 export default Projects
